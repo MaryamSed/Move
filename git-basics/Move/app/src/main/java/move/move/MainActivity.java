799c1852e1b509aa7fc5a0bbc63806f6f4131cc1
Maryam <Sedaghati85@gmail.com>
2020-02-01 22:22:18 +0330

    change move

--- git-basics/Move/app/src/main/java/move/move/MainActivity.java
+++ git-basics/Move/app/src/main/java/move/move/MainActivity.java
@@ -1,13 +1,28 @@
 package move.move;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button start;
+    Context mcontext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(mcontext , MapsActivity.class));
+            }
+        });
+
+
     }
 }
--- git-basics/Move/app/src/main/java/move/move/MapsActivity.java
+++ git-basics/Move/app/src/main/java/move/move/MapsActivity.java
@@ -42,56 +42,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(350739274,510443242)));
         mMap.getUiSettings().setZoomControlsEnabled(true);
-        DetectedActivity detectedActivity ;//= SmartLocation.with(this).activity().getLastActivity();
+        final DetectedActivity detectedActivity ;//= SmartLocation.with(this).activity().getLastActivity();
         detectedActivity = SmartLocation.with(this).activity().getLastActivity();
-      //  final PolygonOptions Line = new PolygonOptions();
-        //Line.strokeWidth(5);
-        //Line.fillColor(Color.RED);
-       // Line.add(new LatLng(35.739274,51.443242));
-        //checkLocationPermissions();
+        final PolygonOptions Line = new PolygonOptions();
+        Line.strokeWidth(5);
+        Line.fillColor(Color.RED);
+        Line.add(new LatLng(35.739274,51.443242));
+        checkLocationPermissions();
 
-   //    for (i=1 ; i<48 ; i++) {
+       for (i=1 ; i<48 ; i++) {
 
-          //  SmartLocation.with(this).location(new LocationBasedOnActivityProvider(
-          //          new LocationBasedOnActivityProvider.LocationBasedOnActivityListener() {
-         //               @Override
-        //                public LocationParams locationParamsForActivity(DetectedActivity detectedActivity) {
-        //                    LocationParams.Builder builder = new LocationParams.Builder();
-        //                    return null;
-         //               }
-        //            }
-         //   ))
-         //           .start(new OnLocationUpdatedListener() {
-          //              @Override
-           //             public void onLocationUpdated(Location location) {
-                           // k=detectedActivity.getType();
-                            //final LatLng mCurrentPlace = new LatLng(location.getLatitude(), location.getLongitude());
-                            //position[i]=mCurrentPlace;
-                            //Line.add(position[i]);
-                            //Polyline polyline = mMap.addPolyline(Line);
-                           // Polyline polyline1 = mMap.addPolyline(Line);
-                          /*  switch (detectedActivity.getType()){
+            SmartLocation.with(this).location(new LocationBasedOnActivityProvider(
+                    new LocationBasedOnActivityProvider.LocationBasedOnActivityListener() {
+                        @Override
+                        public LocationParams locationParamsForActivity(DetectedActivity detectedActivity) {
+                            LocationParams.Builder builder = new LocationParams.Builder();
+                            return null;
+                        }
+                    }
+            ))
+                    .start(new OnLocationUpdatedListener() {
+                        @Override
+                        public void onLocationUpdated(Location location) {
+                            k=detectedActivity.getType();
+                            final LatLng mCurrentPlace = new LatLng(location.getLatitude(), location.getLongitude());
+                            position[i]=mCurrentPlace;
+                            Line.add(position[i]);
+                            switch (detectedActivity.getType()){
                                 case 1 : {
                                     mMap.addMarker(new MarkerOptions().position(position[i])
                                             .icon(BitmapDescriptorFactory.fromResource(R.drawable.buycycle_marker)));
@@ -115,34 +102,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                 default:
                                     mMap.addMarker(new MarkerOptions().position(position[i])
                                             .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
-                            }*/
+                            }
 
 
-    //                    }
-   //                 });
+                        }
+                    });
 
-   //     }
+        }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-       mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
-    //void checkLocationPermissions() {
-    //    Dexter.withActivity(this)
-    //            .withPermissions(
-    //                    Manifest.permission.ACCESS_FINE_LOCATION,
-    //                    Manifest.permission.ACCESS_COARSE_LOCATION
-    //            ).withListener(new MultiplePermissionsListener() {
-    //        @Override
-    //        public void onPermissionsChecked(MultiplePermissionsReport report) {/* ... */}
+    void checkLocationPermissions() {
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                ).withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport report) {/* ... */}
 
-    //        @Override
-    //        public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
 
-    //        }
+            }
 
-    ////    }).check();
-    //}
+       }).check();
+    }
 }
